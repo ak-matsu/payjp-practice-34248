37124762ef31bcf568bae65fb2b23aa848de4ae9
ak-matsu <sandbox.matsu@gmail.com>
2021-04-30 23:17:23 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,10 @@
 const pay = () => {
-  Payjp.setPublickey("pk_test_******************") // PAY.JPテスト公開鍵 今の状態でそのままgithubへプッシュすると悪用されるため、環境変数へ変更する必要がある。
+  // 今の状態でそのままgithubへプッシュすると悪用されるため、環境変数へ変更する必要がある。
+  Payjp.setPublicKey("pk_test_******************"); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
